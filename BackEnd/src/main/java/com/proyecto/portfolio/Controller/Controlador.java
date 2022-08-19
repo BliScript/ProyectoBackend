788ec4f04252ebfa91f138548ef13a2bcacd0f13
BliScript <99600292+BliScript@users.noreply.github.com>
2022-08-19 10:56:19 -0300

    controller

---
+++ BackEnd/src/main/java/com/proyecto/portfolio/Controller/Controlador.java
@@ -0,0 +1,9 @@
+
+package com.proyecto.portfolio.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Controlador {
+    
+}
--- BackEnd/src/main/java/com/proyecto/portfolio/Entity/Persona.java
+++ BackEnd/src/main/java/com/proyecto/portfolio/Entity/Persona.java
@@ -2,8 +2,20 @@
 package com.proyecto.portfolio.Entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Persona {
+
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
 }
