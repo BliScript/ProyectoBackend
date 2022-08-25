93554222aeea288b5fa7721c4c20bbc34ef812e1
BliScript <99600292+BliScript@users.noreply.github.com>
2022-08-25 19:46:38 -0300

    rol

---
+++ BackEnd/src/main/java/com/proyecto/portfolio/Security/Entity/Rol.java
@@ -0,0 +1,23 @@
+
+package com.proyecto.portfolio.Security.Entity;
+
+import com.proyecto.portfolio.Security.Enum.RolNombre;
+import com.sun.istack.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+}
+
---
+++ BackEnd/src/main/java/com/proyecto/portfolio/Security/Enum/RolNombre.java
@@ -0,0 +1,7 @@
+package com.proyecto.portfolio.Security.Enum;
+
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER;
+}
+
